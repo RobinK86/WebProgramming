b1a9f57a249ec4307bb6e04b00390d834ac7f57e
Robin Kelley <rwilkerson@atu.edu>
2026-03-10 16:37:32 -0500

    Added pages to portfolio website, including about, contact, projects, resume, skills, and teaching experience. Updated home page content and styles. Organized previous weeks' assignments into a separate folder.

---
+++ CourseProject/script.js
@@ -0,0 +1,19 @@
+// script.js
+// Robin Kelley
+// 03/10/2026
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    const typed = new Typed(".auto-type", {
+        strings: [
+            "Future Computer Science Educator",
+            "Programming Teaching Assistant",
+            "Lifelong Learner"
+        ],
+        typeSpeed: 50,
+        backSpeed: 30,
+        backDelay: 1500,
+        loop: true
+    });
+
+});
